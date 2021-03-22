1cfc8e12d59cb843018f1e818390293f7e4495fc
11002084 <rzhu.mathleague@gmail.com>
2021-03-22 00:31:58 -0700

    3/22/2020 (3/21/2020)
    Finished Synched Split Loops

--- src/_06_Wait_and_Notify/Practice/SynchedSplitLoops.java
+++ src/_06_Wait_and_Notify/Practice/SynchedSplitLoops.java
@@ -17,29 +17,48 @@ printed in order.
 
 public class SynchedSplitLoops {
 	static int counter = 0;
-	
+	static Object threadLock = new Object();
+
 	public static void main(String[] args) {
 		Thread t1 = new Thread(() -> {
-			for(int i = 0; i < 100000; i++) {
-				counter++;
+			synchronized (threadLock) {
+				for (int i = 0; i < 100000; i++) {
+					try {
+						threadLock.wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					counter++;
+					threadLock.notify();
+				}
 			}
 		});
-		
+
 		Thread t2 = new Thread(() -> {
-			for(int i = 0; i < 100000; i++) {
-				System.out.println(counter);
+			synchronized (threadLock) {
+				for (int i = 0; i < 100000; i++) {
+					System.out.println(counter);
+					threadLock.notify();
+					
+					try {
+						threadLock.wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					
+				}
 			}
 		});
-		
+
 		t1.start();
 		t2.start();
-		
+
 		try {
 			t1.join();
 			t2.join();
 		} catch (InterruptedException e) {
 			System.err.println("Could not join threads");
 		}
-		
+
 	}
 }
